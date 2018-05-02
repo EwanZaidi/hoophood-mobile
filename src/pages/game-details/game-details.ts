417f9e96e5daf93527f8ea0c12d14f82832f4223
Ewan Zaidi <ridhwanzaidi@gmail.com>
2018-05-02 10:39:45 +0800

    changes game details

--- src/pages/game-details/game-details.ts
+++ src/pages/game-details/game-details.ts
@@ -2,7 +2,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, Platform, App, ViewController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFirestoreDocument, AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestoreDocument, AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import { StatisticsPage } from '../statistics/statistics';
 import { ScreenOrientation } from '@ionic-native/screen-orientation';
@@ -17,22 +17,34 @@ export class GameDetailsPage {
   data: any;
   id: any;
 
-  tba : Boolean;
+  tba: Boolean;
   group;
 
-  edit : Boolean = false;
+  edit: Boolean = false;
 
-  constructor(public view: ViewController,public navCtrl: NavController, public navParams: NavParams, private auth : AngularFireAuth, private fs: AngularFirestore, private sc: ScreenOrientation, private platform: Platform, private app: App) {
-    
+  team1_scorer: AngularFirestoreCollection<any>;
+  team1_scorer$: Observable<any>;
+
+  team2_scorer: AngularFirestoreCollection<any>;
+  team2_scorer$: Observable<any>;
+
+  team1_3ptr: AngularFirestoreCollection<any>;
+  team1_3ptr$: Observable<any>;
+
+  team2_3ptr: AngularFirestoreCollection<any>;
+  team2_3ptr$: Observable<any>;
+
+  constructor(public view: ViewController, public navCtrl: NavController, public navParams: NavParams, private auth: AngularFireAuth, private fs: AngularFirestore, private sc: ScreenOrientation, private platform: Platform, private app: App) {
+
   }
 
   ionViewDidLoad() {
     this.id = this.navParams.get('id');
     this.data = this.navParams.get('data');
 
-    if(this.data.team1_name == 'TBA'){
+    if (this.data.team1_name == 'TBA') {
       this.tba = true;
-    }else{
+    } else {
       this.tba = false;
     }
 
@@ -40,29 +52,42 @@ export class GameDetailsPage {
 
     console.log(team_id);
 
-    if(this.data.team1_id == team_id || this.data.team2_id == team_id){
+    if (this.data.team1_id == team_id || this.data.team2_id == team_id) {
       this.edit = true;
-    }else{
+    } else {
       this.edit = false;
     }
 
-    if(this.data.group != undefined){
-    this.splitvalue(this.data.group)}
-  }
-
-  splitvalue(data){
-    if(data.substring(0,2) == 'st'){
-      this.group = data.substring(2,3)
-    }else{
-      this.group = data.substring(3,4)
+    if (this.data.group != undefined) {
+      this.splitvalue(this.data.group)
     }
-    
+
+    this.team1_scorer = this.fs.collection('matches').doc(this.id).collection('team1_scorer', ref => ref.orderBy('points', 'desc'));
+    this.team1_scorer$ = this.team1_scorer.valueChanges();
+
+    this.team2_scorer = this.fs.collection('matches').doc(this.id).collection('team2_scorer', ref => ref.orderBy('points', 'desc'));
+    this.team2_scorer$ = this.team2_scorer.valueChanges();
+
+    this.team1_3ptr = this.fs.collection('matches').doc(this.id).collection('team1_3ptr', ref => ref.orderBy('points', 'desc'));
+    this.team1_3ptr$ = this.team1_3ptr.valueChanges();
+
+    this.team2_3ptr = this.fs.collection('matches').doc(this.id).collection('team2_3ptr', ref => ref.orderBy('points', 'desc'));
+    this.team2_3ptr$ = this.team2_3ptr.valueChanges();
   }
 
-  statsPage(){
+  splitvalue(data) {
+    if (data.substring(0, 2) == 'st') {
+      this.group = data.substring(2, 3)
+    } else {
+      this.group = data.substring(3, 5)
+    }
+
+  }
+
+  statsPage() {
     // this.navCtrl.setRoot(StatisticsPage, {id: this.id, data: this.data})
     this.view.dismiss();
-    this.app.getRootNav().push(StatisticsPage, {id: this.id, data: this.data});
+    this.app.getRootNav().push(StatisticsPage, { id: this.id, data: this.data });
   }
 
 
